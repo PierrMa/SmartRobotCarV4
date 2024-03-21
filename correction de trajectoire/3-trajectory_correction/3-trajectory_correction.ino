ab501169f952322c66627965be133d18b729e88e
Pierr-Marie <pierremariefrederic@gmail.com>
2024-03-21 15:38:28 +0100

    Updated "3-trajectory_correction.ino": -added delay(2000) into mppu_setup() function -modified getdev_angle() function to get a correction deviation angle The proportionnal correction in now operationnal

--- correction de trajectoire/3-trajectory_correction/3-trajectory_correction.ino
+++ correction de trajectoire/3-trajectory_correction/3-trajectory_correction.ino
@@ -13,6 +13,9 @@ unsigned int gyroz_out_h = 0X47;
 unsigned int gyroz_out_l = 0X48;
 float gz0,gz1=0.0;
 
+unsigned long t2=0;
+unsigned int direction = 1;
+
 //function to read one byte in I²C protocol
 int lire_byte_i2c(int addr, int RA){
   int data = 0;
@@ -52,6 +55,7 @@ void mpu_setup(){
   ecrire_byte_i2c(MPU_addr,0X6B,0X09); 
   //configuration du registre du gyroscope (registre 0x1B)
   ecrire_byte_i2c(MPU_addr,0X1B,0b00<<3);
+  delay(2000);//==> VERY IMPORTANT
   //calibrage du gyroscope
   for (int i = 0; i < 100; i++) {
     gz1 = (lire_byte_i2c(MPU_addr,gyroz_out_h)<<8) | (lire_byte_i2c(MPU_addr,gyroz_out_l));
@@ -63,17 +67,15 @@ void mpu_setup(){
 //function to get the deviation angle
 float get_dev_angle(float tol){
   static float dev_cumul = 0.0;
-  unsigned long t1,t2 = 0;
+  unsigned long t1 = 0;
   //enregistrement de la date de début de la mesure
   t1 = millis();
-  //délai pour respecter la fréquence d'échantillonnage du capteur
-  delay (30);
   //lecture de la valeur de la vitesse de rotation angulaire
   gz1 = (lire_byte_i2c(MPU_addr,gyroz_out_h)<<8) | (lire_byte_i2c(MPU_addr,gyroz_out_l));
-  //enregistrement de la date de fin de la mesure
-  t2 = millis();
   //calcul de l'angle de déviation instantanée
-  float dev_inst = (gz1-gz0)/131*(t2-t1)/1000;
+  float dev_inst = (gz1-gz0)/131*(t1-t2)/1000;
+  //enregistrement de la date de fin de la mesure
+  t2=t1;
   //tolérance à l'erreur de 0.05°
   if (fabs(dev_inst)<tol) dev_inst = 0.0;
   //Serial.print("dev_inst: ");
@@ -98,63 +100,52 @@ void loop() {
   float dev_angle; //angle après mouvement
   unsigned int checking_period = 10; //période de réalisation des contrôls en ms
   unsigned long t1,t2=0; //variable pour mesure de durée
-  unsigned long mvt_duration = 2000; //durée du mouvement en ms
-  unsigned int Kp = 10; //paramètre proportionnel du correcteur PID
+  unsigned long mvt_duration = 3000; //durée du mouvement en ms
+  unsigned int Kp = 5; //paramètre proportionnel du correcteur PID
 
   //Relever l'angle de rotation par rapport à l'axe Z avant un mouvement en ligne droite
-  //ref_angle = get_dev_angle(tol);
-  ref_angle = 0;
+  ref_angle = get_dev_angle(tol);
   //relever la date de départ
   t1 = millis();
-  //Faire avancer le robot
-  analogWrite(pwmA,vit);
-  analogWrite(pwmB,vit);
-  digitalWrite(ain1,1);
-  digitalWrite(bin1,1);
-  digitalWrite(stby,1);
   t2 = millis();//pour s'assurer que lors de la première itération, t2-t1<mvt_duration
   while((t2-t1)<mvt_duration){
-    //Toutes les checking_period ms, contrôler la présence d'une déviation
-    delay(checking_period);
     dev_angle = get_dev_angle(tol);
+    Serial.print("dev_angle = ");
+    Serial.println(dev_angle);
     //Corriger la trajectoire si déviation
     float error = dev_angle-ref_angle;
     Serial.print("error = ");
     Serial.println(error);
-    if(error!=0){
-      unsigned int vit_rectif = error*Kp + vit; //nouvelle vitesse à appliquer à l'une des roues
-      //restriction de la variation de vit_rectif à [0;255]
-      if(vit_rectif>255) vit_rectif=255;
-      if(vit_rectif<0) vit_rectif=0;
-      Serial.print("vit_rectif = ");
-      Serial.println(vit_rectif);
-      if(error>0){//si déviation vers la gauche
-        //tourner à droite
-        analogWrite(pwmA,vit_rectif);
-        analogWrite(pwmB,vit);
-        digitalWrite(ain1,1);
-        digitalWrite(bin1,1);
-        digitalWrite(stby,1);
-      }
-      if(error<0){//si déviation vers la droite
-        //tourner à gauche
-        analogWrite(pwmA,vit);
-        analogWrite(pwmB,vit_rectif);
-        digitalWrite(ain1,1);
-        digitalWrite(bin1,1);
-        digitalWrite(stby,1);
-      }
-    }
+    unsigned int vit_rectif_dte = -error*Kp + vit; //nouvelle vitesse à appliquer à la roue droite
+    unsigned int vit_rectif_ghe = error*Kp + vit; //nouvelle vitesse à appliquer à la roue gauche
+    //restriction de la variation de vit_rectif à [0;255]
+    if(vit_rectif_dte>255) vit_rectif_dte=255;
+    if(vit_rectif_dte<10) vit_rectif_dte=10;
+    if(vit_rectif_ghe>255) vit_rectif_ghe=255;
+    if(vit_rectif_ghe<10) vit_rectif_ghe=10;
+    Serial.print("vit_rectif_dte = ");
+    Serial.println(vit_rectif_dte);
+    Serial.print("vit_rectif_ghe = ");
+    Serial.println(vit_rectif_ghe);
+
+    if(direction){
+      analogWrite(pwmA,vit_rectif_dte);
+      analogWrite(pwmB,vit_rectif_ghe);
+      digitalWrite(ain1,direction);
+      digitalWrite(bin1,direction);
+      digitalWrite(stby,1); 
+    }else{
+      analogWrite(pwmA,vit_rectif_ghe);
+      analogWrite(pwmB,vit_rectif_dte);
+      digitalWrite(ain1,direction);
+      digitalWrite(bin1,direction);
+      digitalWrite(stby,1);
+    }    
+    
+    //Toutes les checking_period ms, contrôler la présence d'une déviation
     delay(checking_period);
-    analogWrite(pwmA,vit);
-    analogWrite(pwmB,vit);
-    digitalWrite(ain1,1);
-    digitalWrite(bin1,1);
-    digitalWrite(stby,1);
-    
-    
-    //date de fin de l'intération
+    //date de fin de l'itération
     t2 = millis();
   }
-  Serial.println('2ms');
+  direction = !direction;
 }
